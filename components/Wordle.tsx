541e1d3ca2c486c184656fccfe4b2dc0cd9f8a3b
doughnut1214 <doughnut1214@gmail.com>
2022-11-05 14:21:30 -0400

    API endpoint setup, Wordle changes to call it, CSS changes, awaiting API key

--- components/Wordle.tsx
+++ components/Wordle.tsx
@@ -9,25 +9,38 @@ const Wordle = () => {
         this component should keep track of all things happening in the game, pass the guess up from the guess form, fill the game row 
         with the guesses, change their color if they are right or wrong or missing
     */
-    //todo: set successword from api returning random 4 letter word
-    const [successWord, SetSuccessWord] = useState<string>('STEAMERS')
+    //todo: set successword from api returning random  word
+    const getWord = async () => {
+        const result = await fetch("/api/wotd", {
+            method: "GET", headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        })
+        const data = await result.json()
+        if(!data.ok){
+            console.log("yep no key baby")
+        }
+    }
+    const [successWord, SetSuccessWord] = useState<string>('TESTS')
     const [pastGuesses, SetPastGuesses] = useState<string[]>([])
     const [isWon, SetIsWon] = useState<boolean>(false)
-    let lastGuess = pastGuesses.length -1
-    useEffect(()=>{
+    let lastGuess = pastGuesses.length - 1
+    useEffect(() => {
+        getWord()
         SetPastGuesses([])
-    },[successWord])
+    }, [successWord])
 
     return (
         <div>
-            <Victorybanner hasWon={isWon}/>
+            <Victorybanner hasWon={isWon} />
             <Wordboard target={successWord} recentGuess={pastGuesses[lastGuess]} />
             <Gamerow targetword={successWord} key={1} guess={pastGuesses[0]} />
             <Gamerow targetword={successWord} key={2} guess={pastGuesses[1]} />
             <Gamerow targetword={successWord} key={3} guess={pastGuesses[2]} />
             <Gamerow targetword={successWord} key={4} guess={pastGuesses[3]} />
             <Gamerow targetword={successWord} key={5} guess={pastGuesses[4]} />
-            
+
 
             <Guessform passGuess={SetPastGuesses} length={successWord.length} targetWord={successWord} passWin={SetIsWon} />
         </div>
---
+++ pages/api/wotd.ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+//when api key comes in, change response from any to WOTD
+type WOTD = {
+    //implement me
+}
+type methodError = {
+    message: string
+}
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<WOTD | methodError>
+) {
+    if (req.method === "GET") {
+        const url =`https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${"NYI"}`
+        try {
+            const result = await fetch(`${url}`, {
+                method: "GET", headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            })
+            console.log("internal api status:", result.status)
+            if(!result.ok) throw new Error
+            const data = await result.json()
+            
+            console.log("final data to send: ", data)
+        } catch {
+            res.status(500).json({ message: 'Failure to retrieve data' })
+            return
+        }
+
+        //TODO: json the data returned from the api response
+        res.status(200).json({ name: 'John Doe' })
+    }
+    else {
+        res.status(403).json({ message: 'GET is the only acceptable method' })
+    }
+}
